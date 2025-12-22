0a00ec79c0062c639dff5d016f4205607f69f2f5
Mahdi Ramezani <ramezani200079@gmail.com>
2025-12-22 19:30:30 +0330

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,13 @@
-print("hello world")
+#python class
+
+my_list = [3,4,1,9,7,5,6,10,12,14,20,25]
+
+def number(a):
+    if a > 10:
+        return True
+    else:
+        return False
+
+num = list(filter(number,my_list))
+
+print(num)
